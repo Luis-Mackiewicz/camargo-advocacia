24c50e0149695603509c519769f2c4ed4bb02bfa
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-11 23:47:15 -0300

    refactor: refactoring the header for search engines

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,23 +6,41 @@ export default function Header() {
     "https://wa.me/554499912505?text=Oi%21%20Estou%20entrando%20em%20contato%20com%20a%20Camargo%20Advocacia%20para%20falar%20sobre%20um%20assunto%20juridico.%20Poderiamos%20conversar%3F";
 
   return (
-    <header className="header">
-      <nav>
-        <Link to="/">início</Link>
-        <Link to="/sobre">sobre</Link>
-        <Link to="/servicos">serviços</Link>
-        <Link to="/agendar">agendar</Link>
-        <Link to="/videos">vídeos</Link>
+    <header>
+      <nav className="navigation">
+        <ul className="header-list">
+          <li>
+            {" "}
+            <Link to="/">início</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/sobre">sobre</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/servicos">serviços</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/agendar">agendar</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/videos">vídeos</Link>
+          </li>
+        </ul>
       </nav>
+
       <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
-        <button className="whatsapp-button">
+        {/* <button className="whatsapp-button">
           chame{" "}
           <img
             id="whatsapp-image"
             src="../../public/whatsapp-header.png"
             alt="WhatsApp image"
           />
-        </button>
+        </button> */}
       </a>
     </header>
   );
