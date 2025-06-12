06d51d93db56bbe9c34b7acf72ba25f50535523d
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-11 23:39:07 -0300

    fix: changing component name in about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,9 @@
-import { Home } from "lucide-react";
+import Header from "@/components/Header";
 
 export default function About() {
   return (
     <>
-      <Home />
+      <Header />
     </>
   );
 }
