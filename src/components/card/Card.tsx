93bfcbb077eedcb133534bdcd595fdfecb72750b
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-13 16:46:49 -0300

    feat: creating card components

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,7 +1,33 @@
-import "Card";
+import "./Card.css";
+import { useState } from "react";
 
-const Card = ({ image, title, desciption }) => {
-  return <section></section>;
+interface CardProps {
+  image: string;
+  title: string;
+  description: string;
+}
+
+const Card = ({ image, title, description }: CardProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpaned = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <>
+      <div
+        className={`services-card ${isExpanded ? "expanded" : ""}`}
+        onClick={toggleExpaned}
+      >
+        <img src={image} />
+        <dl>
+          <dt>{title}</dt>
+          {isExpanded && <dd>{description}</dd>}
+        </dl>
+      </div>
+    </>
+  );
 };
 
 export default Card;
