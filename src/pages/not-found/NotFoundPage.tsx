257980a237010d3a3f7d61944b123680b15212f0
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-16 17:11:21 -0300

    feat: creating notFoundPage

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -1,13 +1,28 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import PageContainer from "@/components/container/PageContainer";
+import "./NotFoundPage.css";
 
 export default function NotFoundPage() {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <div>
-        <h1>Not found page</h1>
-        <Link to={"/"} />
-        <button>Go back</button>
+    <PageContainer>
+      <div className="notfound-container">
+        <img
+          src="/not-found.svg"
+          alt="sad dog"
+          width={250}
+          className="notfound-img"
+        />
+        <h1>Opss...</h1>
+        <p>
+          Essa página não foi encontrada. <br />
+          Volte para a página inicial.
+        </p>
+        <button onClick={() => navigate("/")} className="notfound-button">
+          Início
+        </button>
       </div>
-    </>
+    </PageContainer>
   );
 }
