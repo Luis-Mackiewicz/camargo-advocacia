44a20d30dd0eb37ac9fdddb6990882ebb482d165
luis mackiewicz <luismiguelpix@gmail.com>
2025-06-16 16:47:43 -0300

    feat: creating videos page

--- src/pages/videos/Videos.tsx
+++ src/pages/videos/Videos.tsx
@@ -1,10 +1,29 @@
 import "../../components/container/PageContainer";
 import PageContainer from "../../components/container/PageContainer";
+import "./Videos.css";
 
 export default function Videos() {
+  const commingSonImage = "../../../public/coming-soon.svg";
+
   return (
     <>
-      <PageContainer />
+      <PageContainer>
+        <div className="videos-container">
+          <div className="video-text">
+            <h1>Seção de vídeos em breve!</h1>
+            <h2>
+              Estamos prearando uma nova seção com videos exclusívos sobre
+              direito
+            </h2>
+          </div>
+
+          <img
+            src={commingSonImage}
+            alt="comming soon"
+            className="video-image"
+          />
+        </div>
+      </PageContainer>
     </>
   );
 }
